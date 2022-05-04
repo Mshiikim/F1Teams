67cd72faf210ac08aef075af9dbad377f685527e
Mshiikim <66384886+Mshiikim@users.noreply.github.com>
2022-05-04 09:19:11 -0700

    Add project files.

---
+++ F1Teams/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace F1Teams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstTeams = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnMod = new System.Windows.Forms.Button();
+            this.BtnRefresh = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LstTeams
+            // 
+            this.LstTeams.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.LstTeams.HideSelection = false;
+            this.LstTeams.Location = new System.Drawing.Point(18, 26);
+            this.LstTeams.Name = "LstTeams";
+            this.LstTeams.Size = new System.Drawing.Size(239, 179);
+            this.LstTeams.TabIndex = 0;
+            this.LstTeams.UseCompatibleStateImageBehavior = false;
+            this.LstTeams.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TeamID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "TeamName";
+            this.columnHeader2.Width = 170;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(21, 293);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 1;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnMod
+            // 
+            this.BtnMod.Location = new System.Drawing.Point(182, 293);
+            this.BtnMod.Name = "BtnMod";
+            this.BtnMod.Size = new System.Drawing.Size(75, 23);
+            this.BtnMod.TabIndex = 2;
+            this.BtnMod.Text = "Edit";
+            this.BtnMod.UseVisualStyleBackColor = true;
+            // 
+            // BtnRefresh
+            // 
+            this.BtnRefresh.Location = new System.Drawing.Point(21, 340);
+            this.BtnRefresh.Name = "BtnRefresh";
+            this.BtnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.BtnRefresh.TabIndex = 3;
+            this.BtnRefresh.Text = "Refresh";
+            this.BtnRefresh.UseVisualStyleBackColor = true;
+            this.BtnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Location = new System.Drawing.Point(182, 340);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(75, 23);
+            this.BtnDelete.TabIndex = 4;
+            this.BtnDelete.Text = "Delete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(18, 246);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(157, 246);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "TeamID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Team Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 385);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.BtnRefresh);
+            this.Controls.Add(this.BtnMod);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.LstTeams);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView LstTeams;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnMod;
+        private System.Windows.Forms.Button BtnRefresh;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ F1Teams/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+
+
+namespace F1Teams
+{
+    public partial class Form1 : Form
+    {
+        public string connectionString = ConfigurationManager.ConnectionStrings["F1String"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Reload();
+        }
+
+        private void Reload()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string sqlText = "select * from Teams";
+            SqlCommand command = new SqlCommand(sqlText, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            LstTeams.Items.Clear();
+
+            int i = 0;
+            while (reader.Read())
+            {
+                LstTeams.Items.Add(reader["TeamID"].ToString());
+                LstTeams.Items[i].SubItems.Add(reader["TeamName"].ToString());
+                i++;
+            }
+            connection.Close();
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            Reload();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection connect = new SqlConnection(connectionString);
+            connect.Open();
+
+            try
+            {
+                int TeamID = Int32.Parse(txtID.Text);
+                string TeamName = txtName.Text;
+
+                string sqlText = "insert into Teams (TeamID, TeamName)" +
+                    " values (@TeamID, @TeamName)";
+                SqlCommand cmd = new SqlCommand(sqlText, connect);
+                cmd.Parameters.AddWithValue("@TeamID", TeamID);
+                cmd.Parameters.AddWithValue("@Teamname", TeamName);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception) { }
+            connect.Close();
+        }
+    }
+}
